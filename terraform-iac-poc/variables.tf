dee8d23ad45613865129ba5b5dc2e8e96ee4ea75
PremaHosamani1 <prema.hosamani2022@gmail.com>
2026-06-26 15:47:34 +0530

    main variables file

---
+++ terraform-iac-poc/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {}
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_cidr" {}
+
+variable "availability_zone" {}
+
+variable "instance_type" {}
+
+variable "ami" {}
+
+variable "bucket_name" {}
